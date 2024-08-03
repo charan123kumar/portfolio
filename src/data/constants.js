1ec76c6226707c8e7e184f123d53cc614e3c6ebb
charan <chalumurikumar8@gmail.com>
2024-08-03 15:44:58 +0530

    charan

---
+++ src/data/constants.js
@@ -0,0 +1,179 @@
+export const Bio = {
+  name: "Charan Kumar Chalumuri",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/charan123kumar",
+  resume:
+    "https://drive.google.com/file/d/12-vyXvq7ommOtigZauhHc2AXIRwwqXru/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/chalumuri-kumar-8710472b8/",
+  twitter: "",
+  insta: "",
+  facebook: "",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      
+      
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+     
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+     
+     
+      
+      {
+        name: "Java",
+        image:
+          "https://th.bing.com/th?id=OIP.iIXOmGDzrtTJmdwbn7cGMwHaEJ&w=334&h=187&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+     
+    
+    ],
+  },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://th.bing.com/th?id=OIP.3lerszXonPy2hvSP1jkh9wHaHK&w=254&h=245&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+     
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+     
+     
+     {
+      name:"SpringBoot Tool Suite",
+      image:
+        "https://th.bing.com/th?id=OIP.yF0tui-dZARZ5ui6ST3-CQHaD4&w=345&h=181&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
+     }
+    ],
+  },
+];
+
+export const experiences = [
+ 
+  
+  {
+    id: 1,
+    img: "https://th.bing.com/th?id=OIP.DANbNZ1cuM33OqTxeC2dVQHaEo&w=316&h=197&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+    role: "Fullstack Externship",
+    company: "Infosys SpringBoard",
+    date: "March 2024 - June 2024",
+    desc: "Built an EDU HUB full stack web app . I was one of  the top performers in the program.",
+    skills: [
+      "AngularJS",
+     
+      "NodeJs",
+      
+      "HTML",
+      "CSS",
+      "JavaScript",
+      
+     
+      "MySQL",
+    ],
+  },
+  
+  
+ 
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th?id=OIP.ZlMurf2DKMXREae4scVu0QAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+    school: "GITAM UNIVERSITY",
+    date: "Oct 2021 - June 2025",
+    grade: "8.07 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Gandhi Institute of Technology and Management, VisakhaPatnam. I have completed 6 semesters and have a CGPA of 8.07. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems and Operating Systems",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://th.bing.com/th?id=OIP.Ja7xUSMTXHPqbWCfaGkyhgAAAA&w=150&h=150&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
+    school: "Sric Chaitanya",
+    date: "Apr 2019 - March 2021",
+    grade: "92%",
+    desc: "I completed my class 12 high school education at Sri Chaitanya Junior College, Visakhapatnam.",
+    degree: "XII, MPC",
+  },
+  {
+    id: 2,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
+    school: "Naraya Group of Schools, Visakhapatnam",
+    date: "March 2014 - March 2019",
+    grade: "9.8GPA",
+    desc: "I completed my class 10 education at Narayana Group of Schools,Visakhapatnam.",
+    degree: "ICSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  
+  {
+    id: 0,
+    title: "EDU HUB",
+    date: "March 2024 - June 2024",
+    description:
+      "Develop a Online Learning System for Younger Aspirates who are interested for self learning This is a Team Based Project we followed Agile Software Development Process in this project We used  Technologies like Angular Frame Work,Postman,MySQL ,Java and SpringBoot Tool Suite to develop this project",
+    
+    tags: ["HTML", "CSS","Angular", "Java","MySQL"],
+    category: "webapp",
+    github: "https://github.com/SpringBoardMentor115/EduHub",
+   
+    
+  },
+ 
+  
+ 
+  
+];
+
+
